3ffd3de7768d9c0d7c420419cfc63fda5c9c14f0
Maxim <mhuryanov@gmail.com>
2021-10-24 02:30:41 +0300

    Update hero

--- src/views/index1.vue
+++ src/views/index1.vue
@@ -1,53 +1,31 @@
 <script>
-import Navbar from "@/components/navbar";
-import Service from "@/components/services";
-import Features from "@/components/features";
-import Pricing from "@/components/pricing";
-import Blog from "@/components/blog";
-import Contact from "@/components/contact";
-import Footer from "@/components/footer";
+import Navbar from '@/components/navbar';
+import Service from '@/components/services';
+import Features from '@/components/features';
+import Pricing from '@/components/pricing';
+import Blog from '@/components/blog';
+import Contact from '@/components/contact';
+import Footer from '@/components/footer';
+import Hero from '@/components/hero';
 
 /**
  * Index-1
  */
 export default {
-  components: { Navbar, Service, Features, Pricing, Blog, Contact, Footer },
+    components: { Navbar, Service, Features, Pricing, Blog, Contact, Footer, Hero },
 };
 </script>
-<style>
-.section_home {
-  display: flex;
-  justify-content:center;
-  color: white;
-}
-</style>
 <template>
-  <div>
-    <Navbar />
-    <div v-scroll-spy>
-      <!-- Hero Start -->
-      <section class="hero-1-bg" id="home"
-                style="background-color: black; height: 701px"
-      >
-        <div
-          class="container section_home"
-        >
-          <div class="row align-items-center justify-content-center">
-            <div class="col-lg-12 text-center">
-              <h1 class="hero-1-title font-weight-bold text-shadow mb-4">
-                We create Next level <br />Experiences
-              </h1>
-            </div>
-          </div>
+    <div>
+        <Navbar />
+        <div v-scroll-spy>
+            <Hero />
+            <Service />
+            <Features />
+            <Pricing />
+            <Blog />
+            <Contact />
+            <Footer />
         </div>
-      </section>
-      <!-- Hero End -->
-      <Service />
-      <Features />
-      <Pricing />
-      <Blog />
-      <Contact />
-      <Footer />
     </div>
-  </div>
 </template>
